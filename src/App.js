0b0b4201b028e2e813315d54290af904b799c48f
Dabsia <dabojohnson98@gmail.com>
2022-01-02 02:14:25 +0000

    Made a complete functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import Card from './Card'
-import 'tachyons'
-import {robots} from './robots'
+import React, {Component} from 'react';
+import 'tachyons'; 
+import {robots} from './robots';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
 
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
 
-const App = () => {
-  return (
-    <div className="App">
-    <Card id = {robots[0].id} name = {robots[0].name} email = {robots[0].email} />
-    <Card id = {robots[1].id} name = {robots[1].name} email = {robots[1].email} />
-    <Card id = {robots[2].id} name = {robots[2].name} email = {robots[2].email} />
-    </div>
-  );
+  onSearchChange = (event) =>{
+      this.setState({searchfield:event.target.value})
+  }
+  
+  render(){
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLocaleLowerCase()
+      .includes(this.state.searchfield.toLocaleLowerCase())
+    })
+    return (
+      <div className="App">
+        <h1 className='tc'>RoboFriends</h1>
+        <SearchBox searchChange = {this.onSearchChange} />
+        <CardList robots = {filteredRobots}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
+
+
+// In the onsearchChange function, we set the state of the searchfield to be the event.target.value
+
+// we create a variable that filters throuigh the robots array that is going to be filtered through if the condition of the filter method is met
+// Then we pass it as props to the CardList component
+
+// Whenever we type in something into the input box,The onsearchChange function sets the state to that value
---
+++ src/SearchBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// State is the description of your App
+// It is something that can change and affect our App
+// They usually live in the parwnt container
+// The cardlIst accepts the robots as props not as an imported array
+
+const SearchBox = ({searchChange}) => {
+
+    return (
+        <div className="tc" >
+            <input
+                className="pa3 mb5 ba b--green bg-lightest-blue"
+                placeholder="Search Robots"
+                type='search'
+                onChange={searchChange}
+            />
+        </div>
+    )
+}
+
+
+export default SearchBox;
